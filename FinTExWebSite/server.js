1c1cc6fb1c28380206d941e248802c9005f04df6
bhennes2 <bhennes2@gmail.com>
2016-08-01 13:30:18 -0500

    Added configs for local dev server

---
+++ FinTExWebSite/server.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+
+app.use('/css', express.static(__dirname + '/css'));
+app.use('/libs', express.static(__dirname + '/libs'));
+app.use('/font-awesome', express.static(__dirname + '/font-awesome'));
+app.use('/js', express.static(__dirname + '/js'));
+app.use('/img', express.static(__dirname + '/img'));
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+app.listen(8080);
