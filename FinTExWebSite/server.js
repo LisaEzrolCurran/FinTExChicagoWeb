e92084047ddbbd18a0235848531b1991d5ea4723
bhennes2 <bhennes2@gmail.com>
2016-08-02 16:46:45 -0500

    Update geocoding and display of map

--- FinTExWebSite/server.js
+++ FinTExWebSite/server.js
@@ -12,4 +12,6 @@ app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
 });
 
-app.listen(8080);
+app.listen(8080, function () {
+  console.log('Server listening on port 8080');
+});
