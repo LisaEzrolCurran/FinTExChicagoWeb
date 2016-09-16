680093ea105c0bb44cc14fc2e47ac546313313d6
bhennes2 <bhennes2@gmail.com>
2016-09-16 14:05:01 -0500

    Change server port

--- server.js
+++ server.js
@@ -14,6 +14,4 @@ app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
 });
 
-app.listen(8080, function () {
-  console.log('Server listening on port 8080');
-});
+app.set('port', process.env.PORT || 8100);
